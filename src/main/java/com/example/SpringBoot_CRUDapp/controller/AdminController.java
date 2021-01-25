28f181ed50ece6ed6d7daccb3e619fe2fffb5ff7
forte <java1learn@gmail.com>
2021-01-25 20:48:29 +0600

    refactor app.js and controllers

--- src/main/java/com/example/SpringBoot_CRUDapp/controller/AdminController.java
+++ src/main/java/com/example/SpringBoot_CRUDapp/controller/AdminController.java
@@ -8,62 +8,32 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService,
+                           RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
     @GetMapping("")
-    public String getAdmin(){
+    public String printUser(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("login", user.getLogin());
+        model.addAttribute("roles", user.getRoles());
+        model.addAttribute("allRoles", roleService.getAllRoles());
         return "users";
     }
 
-//    private final UserService userService;
-//    private final RoleService roleService;
-//
-//    @Autowired
-//    public AdminController(UserService userService,
-//                           RoleService roleService) {
-//        this.userService = userService;
-//        this.roleService = roleService;
-//    }
-//
-//    @GetMapping("")
-//    public String printUser(Model model) {
-//        List<User> userList = (List<User>) userService.getAllUsers();
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        User user = (User) authentication.getPrincipal();
-//        model.addAttribute("autorize", user);
-//        model.addAttribute("login", user.getLogin());
-//        model.addAttribute("newUser", new User());
-//        model.addAttribute("roles", user.getRoles());
-//        model.addAttribute("users", userList);
-//        model.addAttribute("allRoles", roleService.getAllRoles());
-//        return "users";
-//    }
-//
-//
-//    @PostMapping("/addUser")
-//    public String addUser(@ModelAttribute User newUser) {
-//        userService.createUser(newUser);
-//        return "redirect:/admin";
-//    }
-//
-//    @PostMapping("/edit")
-//    public String editUser(@ModelAttribute User editUser) {
-//        userService.updateUser(editUser);
-//        return "redirect:/admin";
-//    }
-//
-//    @PostMapping("/delete/{id}")
-//    public String deleteUser(@PathVariable("id") long id) {
-//        User user = userService.getUserById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//        userService.deleteUser(user);
-//        return "redirect:/admin";
-//    }
 
 }
--- src/main/java/com/example/SpringBoot_CRUDapp/rest/RestAdminController.java
+++ src/main/java/com/example/SpringBoot_CRUDapp/rest/RestAdminController.java
@@ -1,44 +1,28 @@
 package com.example.SpringBoot_CRUDapp.rest;
 
-import com.example.SpringBoot_CRUDapp.model.Role;
 import com.example.SpringBoot_CRUDapp.model.User;
-import com.example.SpringBoot_CRUDapp.service.RoleService;
 import com.example.SpringBoot_CRUDapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/rest")
 public class RestAdminController {
     private final UserService userService;
-    private final RoleService roleService;
+
 
     @Autowired
-    public RestAdminController(UserService userService, RoleService roleService) {
+    public RestAdminController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
-    }
-
-    @GetMapping("/getAuthorizeUser")
-    public ResponseEntity<User> getAuthUser() {
-        User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return ResponseEntity.ok().body(authUser);
     }
 
     @GetMapping("/getAllUsers")
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok().body(userService.getAllUsers());
-    }
-
-    @GetMapping("/getAllRoles")
-    public ResponseEntity<Set<Role>> getAllRoles() {
-        return ResponseEntity.ok().body(roleService.getAllRoles());
     }
 
     @GetMapping("/getUserById/{id}")
